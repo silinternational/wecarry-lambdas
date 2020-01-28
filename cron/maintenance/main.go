1381ef2f7d0d6c372721f499495e70746296d46b
Schparky <3172830+Schparky@users.noreply.github.com>
2020-01-28 07:55:10 -0700

    make an HTTP call to the API
    use /site/status as a test since it's still set to run every minute

--- cron/maintenance/main.go
+++ cron/maintenance/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -24,5 +27,30 @@ func handler(lambdaConfig LambdaConfig) error {
 	now := time.Now().UTC()
 	log.Printf("WeCarry API Maintenance started at %s", now.Format(time.RFC1123Z))
 
+	request, err := http.NewRequest("GET", os.Getenv("SERVICE_INTEGRATION_URL") + "/site/status", nil)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("SERVICE_INTEGRATION_TOKEN")))
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Println(err)
+	}
+
+	if response.StatusCode >= 300 {
+		log.Printf("unexpected HTTP response code: %s", response.Status)
+	}
+
+	responseBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("error reading response body: %s", err.Error())
+		return nil
+	}
+	log.Printf("response body: %s", string(responseBytes))
+	
 	return nil
 }
